48231c18f43ef7edf1a7c6eab05c6cb4a2476fd7
UlrikeWerner <ulrike_werner@web.de>
2023-10-20 18:47:02 +0200

    scss for the lobby menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss'
 import {Navigate, Routes, Route} from "react-router-dom";
-import Lobby from './pages/Lobby';
-import Game from "./pages/Game.tsx";
+import Lobby from './pages/Lobby/Lobby.tsx';
+import Game from "./pages/Game/Game.tsx";
 
 export default function App() {
 
---
+++ frontend/src/pages/Lobby/Lobby.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import "./lobby.scss";
+
+export default function Lobby() {
+    const navigate = useNavigate();
+
+    function createNewGame() {
+        axios.post("api/game")
+            .then(response => {
+                navigate('/game/' + response.data);
+            })
+            .catch(reason => {
+                console.error(reason);
+            })
+    }
+
+    return (
+        <article className="lobby">
+            <h1>NFL Quartett 2022/23</h1>
+            <div className="football-menu">
+                <div className="football-basic-line-class football-line top-line"></div>
+                <div className="football-basic-line-class football-line bottom-line"></div>
+                <div className="football-basic-line-class football-stitching">
+                    <div className="football-basic-line-class stitch-button-line stitch-button-line">
+                        <button className="football-button" onClick={createNewGame}>
+                            Neues Spiel
+                        </button>
+                    </div>
+                    <div className="football-basic-line-class stitch-button-line stitch-button-2"></div>
+                    <div className="football-basic-line-class stitch-button-line stitch-button-3"></div>
+                </div>
+            </div>
+
+        </article>
+    )
+}
