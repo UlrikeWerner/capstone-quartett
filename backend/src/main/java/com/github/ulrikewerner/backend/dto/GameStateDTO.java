261203e0d9465e19004a84bbb024e923dc5f96da
UlrikeWerner <ulrike_werner@web.de>
2023-10-23 11:17:11 +0200

    add Enum NextTurnBy and GameStateDTO

---
+++ backend/src/main/java/com/github/ulrikewerner/backend/dto/GameStateDTO.java
@@ -0,0 +1,24 @@
+package com.github.ulrikewerner.backend.dto;
+
+import com.github.ulrikewerner.backend.entities.Card;
+import com.github.ulrikewerner.backend.entities.Game;
+import com.github.ulrikewerner.backend.entities.NextTurnBy;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class GameStateDTO {
+    private final Map<String, Integer> score;
+    private final NextTurnBy nextTurnBy;
+    private final Card nextPlayerCard;
+
+    public GameStateDTO(Game game){
+        score = new HashMap<>();
+        score.put("player", game.getPlayerCards().size());
+        score.put("opponent", game.getOpponentCards().size());
+        nextTurnBy = game.isPlayerTurn() ? NextTurnBy.PLAYER : NextTurnBy.OPPONENT;
+        nextPlayerCard = game.getPlayerCards().lookAtFirstCard().orElse(null);
+    }
+}
