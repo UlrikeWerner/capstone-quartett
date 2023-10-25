dd0ef0d6c6e0c194969185e4758788dffc4496a1
UlrikeWerner <ulrike_werner@web.de>
2023-10-25 18:29:49 +0200

    clean up some code smells

--- frontend/src/pages/Game/Component/Card/Card.tsx
+++ frontend/src/pages/Game/Component/Card/Card.tsx
@@ -8,13 +8,41 @@ type CardDeckProps = {
     readonly cardContent?: CardDTO;
     readonly isClickable?: boolean;
     readonly isVisible?: boolean;
-    drawCard?: () => void;
+    readonly drawCard?: () => void;
 }
 
 export default function Card(props: CardDeckProps) {
+
+    function chooseCardType() {
+        if(props.type === "deck"){
+            return <div className={"card card-" + (props.score && props.score > 1 ? props.owner : "isEmpty")}></div>
+        }
+        if(props.type === "movingCard"){
+            return(
+                <div className={"card card-absolute card-" +props.owner + " " +
+                                (props.isClickable ? "card-clickable" : "card-display-none")}
+                                onClick={cardClick}>
+                </div>);
+        }
+
+        return(
+        <div className={props.isVisible ? "card card-absolute card-content border-" + props.owner : "card-display-none"}>
+            <h3>{props.cardContent?.name}</h3>
+            {
+                props.cardContent?.attributes &&
+                Object.entries(props.cardContent.attributes)
+                    .map((attribute: [string, string]) =>
+                        <div className="attributes-wrapper" key={attribute[0]}>
+                            <p>{attribute[0]}</p>
+                            <p>{attribute[1]}</p>
+                        </div>
+                    )
+            }
+        </div>);
+    }
+
     function cardClick() {
         if(props.isClickable){
-            //Todo: Kartenanimation einfügen
             if(props.drawCard){
                 props.drawCard();
             }
@@ -23,31 +51,7 @@ export default function Card(props: CardDeckProps) {
 
     return (
         <>
-            {
-             (props.type === "deck")
-                    ?
-                        <div className={"card card-" + (props.score && props.score > 1 ? props.owner : "isEmpty")}></div>
-                    : (props.type === "movingCard")
-                        ?
-                            <div className={"card card-absolute card-" +props.owner + " " +
-                                (props.isClickable ? "card-clickable" : "card-display-none")}
-                                 onClick={cardClick}>
-                            </div>
-                        :
-                            <div className={props.isVisible ? "card card-absolute card-content border-" + props.owner : "card-display-none"}>
-                                <h3>{props.cardContent?.name}</h3>
-                                {
-                                    props.cardContent?.attributes &&
-                                    Object.entries(props.cardContent.attributes)
-                                        .map((attribute: [string, string]) =>
-                                            <div className="attributes-wrapper" key={attribute[0]}>
-                                                <p>{attribute[0]}</p>
-                                                <p>{attribute[1]}</p>
-                                            </div>
-                                        )
-                                }
-                            </div>
-            }
+            {chooseCardType()}
         </>
     )
 }
