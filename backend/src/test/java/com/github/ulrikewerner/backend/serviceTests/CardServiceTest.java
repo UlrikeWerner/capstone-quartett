8df09b54f8c4ec9d71d50a7b1758ba6a0f2ae0af
UlrikeWerner <ulrike_werner@web.de>
2023-10-27 20:28:32 +0200

    add tests for the compare methode in the CardServiceTest

--- backend/src/test/java/com/github/ulrikewerner/backend/serviceTests/CardServiceTest.java
+++ backend/src/test/java/com/github/ulrikewerner/backend/serviceTests/CardServiceTest.java
@@ -3,6 +3,8 @@ package com.github.ulrikewerner.backend.serviceTests;
 import com.github.ulrikewerner.backend.entities.Card;
 import com.github.ulrikewerner.backend.entities.CardAttribute;
 import com.github.ulrikewerner.backend.entities.NflLogoAcronym;
+import com.github.ulrikewerner.backend.entities.TurnWinner;
+import com.github.ulrikewerner.backend.exception.CategoryNotFoundException;
 import com.github.ulrikewerner.backend.repositories.CardRepo;
 import com.github.ulrikewerner.backend.services.CardService;
 import org.junit.jupiter.api.Test;
@@ -44,5 +46,108 @@ class CardServiceTest {
 
         verify(cardRepo).findAll();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void compare_expectTurnWinnerPlayer_WhenIsBiggerBetterIsTrueAndThePlayerCardIsHigher() throws CategoryNotFoundException {
+        CardAttribute more = new CardAttribute("test", 12, true, true);
+        CardAttribute more2 = new CardAttribute("test", 1212, false, true);
+        CardAttribute less = new CardAttribute("test", 2, true, true);
+        CardAttribute less2 = new CardAttribute("test", 212, false, true);
+        Card moreCard = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(more)));
+        Card moreCard2 = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(more2)));
+        Card lessCard = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(less)));
+        Card lessCard2 = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(less2)));
+
+        TurnWinner actual = cardService.compare(moreCard, lessCard, "test");
+        TurnWinner actual2 = cardService.compare(moreCard2, lessCard2, "test");
+
+        assertEquals(TurnWinner.PLAYER, actual);
+        assertEquals(TurnWinner.PLAYER, actual2);
+    }
+
+    @Test
+    void compare_expectTurnWinnerOpponent_WhenIsBiggerBetterIsTrueAndTheOpponentCardIsHigher() throws CategoryNotFoundException {
+        CardAttribute more = new CardAttribute("test", 12, true, true);
+        CardAttribute more2 = new CardAttribute("test", 1212, false, true);
+        CardAttribute less = new CardAttribute("test", 2, true, true);
+        CardAttribute less2 = new CardAttribute("test", 212, false, true);
+        Card moreCard = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(more)));
+        Card moreCard2 = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(more2)));
+        Card lessCard = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(less)));
+        Card lessCard2 = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(less2)));
+
+        TurnWinner actual = cardService.compare(lessCard, moreCard, "test");
+        TurnWinner actual2 = cardService.compare(lessCard2, moreCard2, "test");
+
+        assertEquals(TurnWinner.OPPONENT, actual);
+        assertEquals(TurnWinner.OPPONENT, actual2);
+    }
+
+    @Test
+    void compare_expectTurnWinnerDraw_WhenIsBiggerBetterIsTrueAndTheCardsAreEqual() throws CategoryNotFoundException {
+        CardAttribute attribute = new CardAttribute("test", 12, true, true);
+        CardAttribute attribute2 = new CardAttribute("test", 1212, false, true);
+        Card cardWithAttribute1Version1 = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(attribute)));
+        Card cardWithAttribute2Version1 = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(attribute2)));
+        Card cardWithAttribute1Version2 = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(attribute)));
+        Card cardWithAttribute12Version2 = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(attribute2)));
+
+        TurnWinner actual = cardService.compare(cardWithAttribute1Version1, cardWithAttribute1Version2, "test");
+        TurnWinner actual2 = cardService.compare(cardWithAttribute2Version1, cardWithAttribute12Version2, "test");
+
+        assertEquals(TurnWinner.DRAW, actual);
+        assertEquals(TurnWinner.DRAW, actual2);
+    }
+    @Test
+    void compare_expectTurnWinnerPlayer_WhenIsBiggerBetterIsFalseAndThePlayerCardIsLess() throws CategoryNotFoundException {
+        CardAttribute more = new CardAttribute("test", 12, true, false);
+        CardAttribute more2 = new CardAttribute("test", 1212, false, false);
+        CardAttribute less = new CardAttribute("test", 2, true, false);
+        CardAttribute less2 = new CardAttribute("test", 212, false, false);
+        Card moreCard = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(more)));
+        Card moreCard2 = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(more2)));
+        Card lessCard = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(less)));
+        Card lessCard2 = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(less2)));
+
+        TurnWinner actual = cardService.compare(lessCard, moreCard, "test");
+        TurnWinner actual2 = cardService.compare(lessCard2, moreCard2, "test");
+
+        assertEquals(TurnWinner.PLAYER, actual);
+        assertEquals(TurnWinner.PLAYER, actual2);
+    }
+
+    @Test
+    void compare_expectTurnWinnerOpponent_WhenIsBiggerBetterIsFalseAndTheOpponentCardIsLess() throws CategoryNotFoundException {
+        CardAttribute more = new CardAttribute("test", 12, true, false);
+        CardAttribute more2 = new CardAttribute("test", 1212, false, false);
+        CardAttribute less = new CardAttribute("test", 2, true, false);
+        CardAttribute less2 = new CardAttribute("test", 212, false, false);
+        Card moreCard = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(more)));
+        Card moreCard2 = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(more2)));
+        Card lessCard = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(less)));
+        Card lessCard2 = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(less2)));
+
+        TurnWinner actual = cardService.compare(moreCard, lessCard, "test");
+        TurnWinner actual2 = cardService.compare(moreCard2, lessCard2,"test");
+
+        assertEquals(TurnWinner.OPPONENT, actual);
+        assertEquals(TurnWinner.OPPONENT, actual2);
+    }
+
+    @Test
+    void compare_expectTurnWinnerDraw_WhenIsBiggerBetterIsFalseAndTheCardsAreEqual() throws CategoryNotFoundException {
+        CardAttribute attribute = new CardAttribute("test", 12, true, false);
+        CardAttribute attribute2 = new CardAttribute("test", 1212, false, false);
+        Card cardWithAttribute1Version1 = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(attribute)));
+        Card cardWithAttribute2Version1 = new Card("1", "testCard1", NflLogoAcronym.ARI, new ArrayList<>(List.of(attribute2)));
+        Card cardWithAttribute1Version2 = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(attribute)));
+        Card cardWithAttribute12Version2 = new Card("2", "testCard2", NflLogoAcronym.NO, new ArrayList<>(List.of(attribute2)));
+
+        TurnWinner actual = cardService.compare(cardWithAttribute1Version1, cardWithAttribute1Version2, "test");
+        TurnWinner actual2 = cardService.compare(cardWithAttribute2Version1, cardWithAttribute12Version2, "test");
+
+        assertEquals(TurnWinner.DRAW, actual);
+        assertEquals(TurnWinner.DRAW, actual2);
     }
 }
