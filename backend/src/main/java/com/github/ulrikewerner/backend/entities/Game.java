30551e20816606bb7b1de153ce0f040aac221b03
UlrikeWerner <ulrike_werner@web.de>
2023-11-07 18:02:39 +0100

    update the entity game

--- backend/src/main/java/com/github/ulrikewerner/backend/entities/Game.java
+++ backend/src/main/java/com/github/ulrikewerner/backend/entities/Game.java
@@ -2,18 +2,27 @@ package com.github.ulrikewerner.backend.entities;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 public class Game {
     private String id;
+    private LocalDateTime date;
+    private String title;
     private boolean isPlayerTurn;
     private Deck playerCards;
     private Deck opponentCards;
     private boolean isFinished;
 
     public Game(Deck playerCards, Deck opponentCards) {
+        date = LocalDateTime.now();
+        DateTimeFormatter dateFormatObject = DateTimeFormatter.ofPattern("dd.MM.yy, HH:mm");
+        String formattedDate = date.format(dateFormatObject);
+        title = "Spiel vom " + formattedDate;
         isPlayerTurn = true;
         this.playerCards = playerCards;
         this.opponentCards = opponentCards;
