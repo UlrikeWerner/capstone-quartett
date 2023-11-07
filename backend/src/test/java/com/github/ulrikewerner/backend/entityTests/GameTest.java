012938067c9f9fb10f71e0b3231a9b9952eefa8c
UlrikeWerner <ulrike_werner@web.de>
2023-11-07 18:09:46 +0100

    expand GameTest with the fields: date and title

--- backend/src/test/java/com/github/ulrikewerner/backend/entityTests/GameTest.java
+++ backend/src/test/java/com/github/ulrikewerner/backend/entityTests/GameTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +24,15 @@ class GameTest {
         Deck opponent = new Deck(cardList2);
 
         Game testGame = new Game(player, opponent);
+        LocalDateTime testDate = LocalDateTime.now();
+        DateTimeFormatter dateFormatObject = DateTimeFormatter.ofPattern("dd.MM.yy, HH:mm");
+        String formattedDate = testDate.format(dateFormatObject);
+        String testTitle = "Spiel vom " + formattedDate;
 
         assertNull(testGame.getId());
+        assertNotNull(testGame.getDate());
+        assertNotNull(testGame.getTitle());
+        assertEquals(testTitle, testGame.getTitle());
         assertTrue(testGame.isPlayerTurn());
         assertEquals(testGame.getPlayerCards(), player);
         assertEquals(testGame.getOpponentCards(), opponent);
