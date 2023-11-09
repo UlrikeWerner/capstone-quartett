2a265f3bdff10c7f80eab8b286fdbc627f997068
UlrikeWerner <ulrike_werner@web.de>
2023-11-09 17:48:16 +0100

    add editing method for changing title of the game

--- frontend/src/pages/GameOverview/GameOverview.tsx
+++ frontend/src/pages/GameOverview/GameOverview.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {FormEvent, useEffect, useState} from "react";
 import axios from "axios";
 import {OpenGameDTO} from "../../types/OpenGameDTO.ts";
 import {useNavigate} from "react-router-dom";
@@ -9,6 +9,8 @@ export default function GameOverview() {
     const navigate = useNavigate();
     const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
     const [gameList, setGameList] = useState<OpenGameDTO[]>([]);
+    const [idToEdit, setIdToEdit] = useState<string>("");
+    const [titleValue, setTitleValue] = useState<string>();
 
     useEffect(() => {
         getGameList()
@@ -24,16 +26,41 @@ export default function GameOverview() {
             });
     }
 
+    function submitNewTitle(event: FormEvent, gameId: string, newTitle: string) {
+        event.preventDefault();
+        axios.patch(`/api/game/${gameId}`, {
+            title: newTitle
+        })
+            .then(() => {
+                setTitleValue("");
+                setIdToEdit("");
+                getGameList();
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
     function deleteGame(gameId: string) {
-        if(!window.confirm("Wirklich löschen?")){
+        if (!window.confirm("Wirklich löschen?")) {
             return;
         }
         axios.delete("/api/game/" + gameId)
             .then(() => {
                 getGameList();
             }).catch(() => {
-                setErrorMessage("Beim Löschen ist was schief gelaufen!");
+            setErrorMessage("Beim Löschen ist was schief gelaufen!");
         })
+    }
+
+    function startEditing(id: string, title: string) {
+        setIdToEdit(id);
+        setTitleValue(title);
+    }
+
+    function cancelEditing() {
+        setIdToEdit("");
+        setTitleValue("");
     }
 
     return (
@@ -47,16 +74,49 @@ export default function GameOverview() {
                     {
                         gameList.map((game) =>
                             <div className="game-element-wrapper" key={game.gameId}>
-                                <span className="game-title">{game.title}</span>
+                                {idToEdit === game.gameId
+                                    ?
+                                    <form className="edit-form">
+                                        <input className="input-title"
+                                               type="text"
+                                               value={titleValue}
+                                               onChange={event => setTitleValue(event.target.value)}
+                                        />
+                                        <button className="button icon-button"
+                                                type="submit"
+                                                title="Speichern"
+                                                onClick={(event: FormEvent) => {
+                                                    if (titleValue) {
+                                                        submitNewTitle(event, game.gameId, titleValue)
+                                                    }
+                                                }}
+                                        >
+                                            check
+                                        </button>
+
+                                        <BasicButton icon={true}
+                                                     tooltip="Abbrechen"
+                                                     text="close"
+                                                     buttonClick={() => cancelEditing()}
+                                        />
+                                    </form>
+                                    :
+                                    <span className="game-title">{game.title}</span>
+                                }
                                 <div className="button-wrapper">
                                     <BasicButton text="Spielen"
                                                  buttonClick={() => navigate('/game/' + game.gameId)}
                                     />
-                                    <BasicButton text="close"
+                                    <BasicButton text="edit"
+                                                 icon={true}
+                                                 tooltip="bearbeiten"
+                                                 buttonClick={() => startEditing(game.gameId, game.title)}
+                                    />
+                                    <BasicButton text="delete"
                                                  icon={true}
                                                  tooltip="löschen"
                                                  functionValue={game.gameId}
-                                                 buttonClick={(value) => deleteGame(value)}
+                                                 buttonClick={(value: string) => deleteGame(value)}
                                     />
                                 </div>
                             </div>
